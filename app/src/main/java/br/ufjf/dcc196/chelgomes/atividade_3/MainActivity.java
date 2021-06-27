86f6b04b387e0f9c93f10a14df118fe6a92f3d82
Michel Gomes <michel.andrade@estudante.ufjf.br>
2021-06-27 00:02:46 -0300

    criacao da funcao soma

--- app/src/main/java/br/ufjf/dcc196/chelgomes/atividade_3/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/chelgomes/atividade_3/MainActivity.java
@@ -3,12 +3,38 @@ package br.ufjf.dcc196.chelgomes.atividade_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextPrimeiroNumero;
+    private EditText editTextSegundoNumero;
+    private TextView textViewResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextPrimeiroNumero = findViewById(R.id.editTextPrimeiroNumero);
+        editTextSegundoNumero = findViewById(R.id.editTextSegundoNumero);
+        textViewResultado = findViewById(R.id.textViewResultado);
     }
+
+    public void calculoSoma(View view){
+        Double calculoNumero1 = Double.parseDouble(editTextPrimeiroNumero.getText().toString());
+        Double calculoNumero2 = Double.parseDouble(editTextSegundoNumero.getText().toString());
+
+        Double Soma = calculoNumero1 + calculoNumero2;
+
+        textViewResultado.setText(Soma.toString());
+
+    }
+
+    /*Double Subtracao = calculoNumero1 - calculoNumero2;
+    textViewResultado.setText(Subtracao.toString());*/
 }
